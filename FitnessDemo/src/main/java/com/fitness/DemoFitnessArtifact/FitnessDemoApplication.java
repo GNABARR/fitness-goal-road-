686e334e0cbede5b6f1a29ecd94983d3ac1af265
GNABARR <162433630+GNABARR@users.noreply.github.com>
2026-01-07 16:47:48 +0100

    fix problems of backend, now i think all is fine as a initialisation for the project before dev

---
+++ FitnessDemo/src/main/java/com/fitness/DemoFitnessArtifact/FitnessDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.DemoFitnessArtifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessDemoApplication.class, args);
+	}
+
+}

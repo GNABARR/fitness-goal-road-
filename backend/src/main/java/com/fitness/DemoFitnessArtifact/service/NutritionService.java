6e8a0600a6f7c94cc79c62f06abe4b6cd3bc4ab7
Emmka1412 <157417624+Emmka1412@users.noreply.github.com>
2026-01-23 15:06:12 +0100

    4eme

--- backend/src/main/java/com/fitness/DemoFitnessArtifact/service/NutritionService.java
+++ backend/src/main/java/com/fitness/DemoFitnessArtifact/service/NutritionService.java
@@ -13,17 +13,17 @@ public class NutritionService {
     public NutritionResponse calculateNutrition(User user) {
         int age = Period.between(user.getDateOfBirth(), LocalDate.now()).getYears();
         
-        // Calcul BMR (Mifflin-St Jeor pour hommes)
+        
         int bmr = (int) Math.round(
             10 * user.getWeight() + 
             6.25 * user.getHeight() - 
             5 * age + 5
         );
         
-        // Calcul TDEE
+        
         int tdee = (int) Math.round(bmr * user.getActivityLevel());
         
-        // Ajustement selon l'objectif
+        
         int calories = tdee;
         if ("cutting".equals(user.getGoal())) {
             calories = tdee - 500;
@@ -31,7 +31,7 @@ public class NutritionService {
             calories = tdee + 300;
         }
         
-        // Calcul des macros
+        
         int protein = (int) Math.round(user.getWeight() * 2.2);
         int fats = (int) Math.round((calories * 0.25) / 9);
         int carbs = (int) Math.round((calories - (protein * 4) - (fats * 9)) / 4);

628101f9c8efa26409718225e366dadb027e922d
Aymane MARZOUK <aymanemarzouk222@gmail.com>
2026-03-03 17:20:30 +0100

    modif bd

--- backend/src/main/java/com/protosirius/backend/entity/Measure.java
+++ backend/src/main/java/com/protosirius/backend/entity/Measure.java
@@ -2,14 +2,35 @@ package com.protosirius.backend.entity;
 
 import java.time.Instant;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 
+
+@Entity
+@Table(name = "MesuresBMI")
 public class Measure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column
     private double poidsKg;
+
+    @Column
     private double tailleCm;
+
+    @Column
     private int bmi;
+
+    @Column
     private String categorie;
+
+    @Column
     private Instant date;
 
     public Measure() {
